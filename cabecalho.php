77832bea86d96b970abe321377e9a4a95ac02ae1
Camila Uemura <80077791+uemuracamila@users.noreply.github.com>
2021-05-04 19:23:53 -0300

    Cabeçalho e Rodapé para usar include

---
+++ cabecalho.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:400,300,700" rel="stylesheet" 
+    type="text.css">
+    <link rel="shortcut icon" href="icones/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="estilos.css">
+    </head>
+
+<header class="w3-white">    
+    <div class="w3-center">
+        <nav class="w3-drodown-hover w3-center w3-mobile">
+                    <img src="logos/logo.jpg" alt="Logo Amor de quatro patas." class="w3-image" width="10%">
+
+                    <a href="#home" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-home w3-xlarge"></i>
+                        <p>HOME</p>
+                    </a>
+                    <a href="formularioadocao.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-heart-o w3-xlarge"></i>
+                        <p>Adote</p>
+                    </a>
+                    <a href="animais.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-paw w3-xlarge"></i>
+                        <p>Animais</p>
+                    </a>
+                    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-comments-o w3-xlarge"></i>
+                        <p>Contato</p>
+                    </a>
+                    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-money w3-xlarge"></i>
+                        <p>Doe</p>
+                    </a>
+                    <a href="noticias.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="	fa fa-newspaper-o	 w3-xlarge"></i>
+                        <p>Notícias</p>
+                    </a>
+                    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="	fa fa-thumbs-o-up w3-xlarge"></i>
+                        <p>Parceiros</p>
+                    </a>
+                    <!-- <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-user w3-xlarge"></i>
+                        <p>Produtos</p>
+                    </a> -->
+                    <a href="equipe.html" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-group w3-xlarge"></i>
+                        <p>Equipe</p>
+                    </a>
+                    <!-- <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="	fa fa-question w3-xlarge"></i>
+                        <p>Quem Somos</p>
+                    </a> -->
+                    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-hover-black w3-hover-text-white w3-text-black">
+                        <i class="fa fa-user w3-xlarge"></i>
+                        <p>Seja Voluntário</p>
+                    </a>                    
+        </nav>
+    </div>
+</header>
---
+++ rodape.php
@@ -0,0 +1,20 @@
+<footer class="rodape">
+        <div class="social-icons">
+            <a href="#" class="btn-facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="btn-youtube"><i class="fa fa-youtube"></i></a>
+            <a href="#" class="btn-instagram"><i class="fa fa-instagram"></i></a>
+        </div>
+        <p class="copyright">
+            Copyright &#0169 &#x2764;&#128062;&#128062; 2021.
+        </p>
+    </footer>
+    <script src="http://code.jquery.com/jquery-1.12.0.min.js">
+    </script>
+    <script>
+        $(".btn-menu").click(function () {
+            $(".menu").show();
+        })
+        $(".btn-fechar").click(function () {
+            $(".menu").hide();
+        })
+    </script>
