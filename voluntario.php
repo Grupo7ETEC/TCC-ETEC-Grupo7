4955715a901e331245eb2f1365a5d4bd7b802abb
Camila Uemura <80077791+uemuracamila@users.noreply.github.com>
2021-05-04 21:39:24 -0300

    Add files via upload

---
+++ voluntario.php
@@ -0,0 +1,30 @@
+<?php
+    include("cabecalho.php");
+?>
+
+<title>Seja voluntário</title>
+<body class="w3-white">
+    <div class="w3-row-padding w3-margin-top">
+        <div class="w3-third">  
+                <img src="img/gato.jpg" style="width:100%">
+        </div>
+        <div class="w3-third">
+            <img src="img/banho.jpg" style="width:100%">
+        </div>
+        <div class="w3-third">  
+                <img src="img/carinho.jpg" style="width:100%">
+        </div>
+    </div>
+
+    <div class="banner"> 
+        <h2>Seja um voluntário</h2>
+        <h3>Se você gosta de animais e tem vontade de ajudar na causa animal, a Casa de Adoção Amor de 4 Patas pode ser o lugar para você. Nossas voluntárias e voluntários ajudam de diversas formas, como feiras de adoção, dia do banho, brincando com os animais no abrigo e muito mais. Para participar, basta preencher o formulário de cadastro. Quando necessário, entraremos em contato para verificar sua disponibilidade. Ao final de cada ano, você receberá um certificado com a atividade que realizou!</h3>    
+        <br>
+
+        <a href="formulariovoluntario.html">
+            <button class="w3-btn w3-center w3-green w3-round w3-sans-serif w3-xlarge">Quero ser voluntário!</button></a>
+    </div>
+
+<?php
+    include("rodape.php");
+?>
