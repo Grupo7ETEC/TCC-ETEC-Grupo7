0963fb8c759ef5aa58faa94e8715271c2e640939
Camila Uemura <80077791+uemuracamila@users.noreply.github.com>
2021-05-05 18:02:40 -0300

    Update voluntario.php

--- voluntario.php
+++ voluntario.php
@@ -21,7 +21,7 @@
         <h3>Se você gosta de animais e tem vontade de ajudar na causa animal, a Casa de Adoção Amor de 4 Patas pode ser o lugar para você. Nossas voluntárias e voluntários ajudam de diversas formas, como feiras de adoção, dia do banho, brincando com os animais no abrigo e muito mais. Para participar, basta preencher o formulário de cadastro. Quando necessário, entraremos em contato para verificar sua disponibilidade. Ao final de cada ano, você receberá um certificado com a atividade que realizou!</h3>    
         <br>
 
-        <a href="formulariovoluntario.html">
+        <a href="formulariovoluntario.php">
             <button class="w3-btn w3-center w3-green w3-round w3-sans-serif w3-xlarge">Quero ser voluntário!</button></a>
     </div>
 </body>
