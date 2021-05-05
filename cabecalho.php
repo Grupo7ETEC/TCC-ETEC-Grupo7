b7b6bc2a3742589abfd36f2fbaf3542bd56299a0
Camila Uemura <80077791+uemuracamila@users.noreply.github.com>
2021-05-04 21:35:39 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:400,300,700" rel="stylesheet" 
